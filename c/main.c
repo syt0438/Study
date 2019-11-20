1d99e294bcdee0314db1b0d076f8193a1a3398d2
syt0438 <syt0438@gmail.com>
2019-11-20 15:44:01 +0800

    linked_demo_01

--- c/main.c
+++ c/main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Island
+{
+    char *name;
+    char *opens;
+    char *closes;
+    struct Island *next;
+} Island;
+
 int main()
 {
-    
+    //#region init data
+    Island amity = {"Amity", "09:00", "17:00", NULL};
+    Island craggy = {"Craggy", "09:00", "17:00", NULL};
+    Island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
+    Island shutter = {"Shutter", "09:00", "17:00", NULL};
+
+    amity.next = &craggy;
+    craggy.next = &isla_nublar;
+    isla_nublar.next = &shutter;
+    //#endregion
+
+    //#region loop
+    Island *next = &amity;
+    int index = 0;
+
+    while (next != NULL)
+    {
+        printf("[%d]: name: %s, opens: %s, closes: %s\n", ++index, next->name, next->opens, next->closes);
+
+        next = next->next;
+    }
+    //#endregion
 
     return 0;
 }
