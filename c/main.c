7d6c3ca946ade5a61d8633cdcfb4fc42afa04345
syt0438 <syt0438@gmail.com>
2019-11-27 09:46:24 +0800

    strerror_errno

--- c/main.c
+++ c/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
-    
+    puts(strerror(errno));
+    int ret = execl("diner_info", "diner_info", "4", NULL);
+    printf("ret = %d, errno = %d\n", ret, errno);
+    puts(strerror(errno));
 
     return EXIT_SUCCESS;
 }
