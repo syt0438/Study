605ee8047a298b98912c290cb21b67c4b0276cb1
syt0438 <syt0438@gmail.com>
2019-12-05 09:01:39 +0800

    server_01

--- c/11/socket_util.c
+++ c/11/socket_util.c
@@ -45,15 +45,6 @@ int say(int fd, char *msg)
     return ret;
 }
 
-/*
- * 字符串不以 \ 0 结尾。
- * 
- * 当用户在 telnet 输入文本时，字符串以 \ r\n 结尾。
- * 
- * recv() 将返回字符个数，如果发生错误就返回 - 1，如果客户端关闭了连接，就返回 0。
- * 
- * recv() 调用不一定能一次接收到所有字符。
- */
 int read_in(int fd, char *buf, int len)
 {
     char *s = buf;
--- c/11/socket_util.h
+++ c/11/socket_util.h
@@ -7,4 +7,13 @@ void bind_to_port(int fd, int port);
 
 int say(int fd, char *msg);
 
+/*
+ * 字符串不以 \ 0 结尾。
+ * 
+ * 当用户在 telnet 输入文本时，字符串以 \r\n 结尾。
+ * 
+ * recv() 将返回字符个数，如果发生错误就返回 - 1，如果客户端关闭了连接，就返回 0。
+ * 
+ * recv() 调用不一定能一次接收到所有字符。
+ */
 int read_in(int fd, char *buf, int len);
